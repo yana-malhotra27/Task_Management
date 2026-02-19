816b323a758300eedc8f06ef4e1b703d70f77f77
yana-malhotra27 <yana27.ym@gmail.com>
2026-02-19 11:19:07 +0530

    ui

---
+++ public/script/main.js
@@ -0,0 +1,9 @@
+  function searchTasks() {
+    const input = document.getElementById('searchTask').value.toLowerCase();
+    const cards = document.querySelectorAll('.task-card');
+
+    cards.forEach(card => {
+      const title = card.querySelector('.card-title').innerText.toLowerCase();
+      card.style.display = title.includes(input) ? '' : 'none';
+    });
+  }
